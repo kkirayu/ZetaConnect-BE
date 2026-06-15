9e5cc831369bfebbc28a3c73bfdfb9a36df79b68
naufal murtadho <naufalmurtadho202@gmail.com>
2026-06-15 18:48:03 +0700

    add:route, handler in pharmacy dashboard

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PharmacyDashboardController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,3 +19,12 @@ Route::apiResource('appointments', AppointmentController::class);
 // Google OAuth Routes
 Route::get('/auth/google', [AuthController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
+
+Route::prefix('pharmacy')->group(function () {
+
+    Route::get('/dashboard', [PharmacyDashboardController::class, 'dashboard']);
+
+    Route::get('/low-stock', [PharmacyDashboardController::class, 'lowStock']);
+
+    Route::get('/patient-demographics', [PharmacyDashboardController::class, 'patientDemographics']);
+});
